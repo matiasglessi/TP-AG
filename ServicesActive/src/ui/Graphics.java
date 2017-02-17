fb74ee85b3fed2abd3ca61d565d5968888bc4a84
Matias Glessi <matiasglessi@gmail.com>
2017-02-17 20:50:20 -0300

    add starter graphics display

--- ServicesActive/src/ui/Graphics.java
+++ ServicesActive/src/ui/Graphics.java
@@ -1,5 +1,14 @@
 package ui;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
 public class Graphics {
 
+	public Graphics (){
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(null);
+
+	}
 }
